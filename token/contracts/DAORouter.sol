bb82761c96b38a62adf606195286fb744f3ae1db
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2022-03-15 20:30:34 +0400

    Add governor contract

--- token/contracts/DAORouter.sol
+++ token/contracts/DAORouter.sol
@@ -2,6 +2,16 @@ pragma solidity ^0.8.7;
 
 contract DAORouter {
     
+    /**
+    
+        What's needed?
+
+        1. Tokenomics
+        2. Governor
+        3. Create new property (with governor)
+        4. ...
+    
+     */
     constructor () {
     }
 
