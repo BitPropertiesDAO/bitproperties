0246a5342cd211a21c37e70fc683806bec5e3c73
Andrew Chen <andrewrchen05@gmail.com>
2022-04-04 09:42:22 -0700

    Create Basic Info card

---
+++ app/src/Components/DAO/Forms/FormItem.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import { InputNumber, Input } from "antd";
+
+interface Props {
+    inputTitle?: string,
+    placeHolder?: string,
+    disabled?: boolean
+}
+
+const defaultProps = {
+    inputTitle: "Form Title",
+    placeHolder: "Placeholder",
+    diabled: false
+}
+
+const FormItem: React.FC<Props> = (props: Props) => {
+
+    return(
+        <>
+        <div>
+            <strong 
+            style={{color: '#ffffff'}}
+            >
+                {props.inputTitle}
+            </strong>
+        </div>
+        <div>
+        <Input
+            disabled={props.disabled}
+            maxLength={25}
+            // style={{ width: 300 }}
+            type="string"
+            value={props.placeHolder}
+            //   onChange={(e) => dispatch(changeTokenName(String(e.target.value)))}
+            className="alchemy--input"
+        />
+        </div>
+        
+        </>
+    )
+}
+
+FormItem.defaultProps = defaultProps
+export default FormItem
